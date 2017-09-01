e201bac0d5d9f0d4acd89eed21d185b1f2f1c63b
Brendan Azzano <brendanazzano@gmail.com>
2017-09-01 02:38:10 +0000

    Created signup form and validations.

--- app/actions.rb
+++ app/actions.rb
@@ -1,6 +1,27 @@
 get '/' do 
-    
     @posts = Post.order(created_at: :desc)
     erb(:index)
+end
 
+get '/signup' do            # if user goes to signup
+    @user = User.new        # setup empty @user object
+    erb(:signup)            # render app/views/signup.erb
+end
+
+post '/signup' do
+    #grab form entry
+    email = params[:email]
+    avatar_url = params[:avatar_url]
+    username = params[:username]
+    password = params[:password]
+    
+    #initiate a user
+    @user = User.new({email: email, avatar_url: avatar_url, username: username, password: password})
+    
+    #if validations pass, save user
+    if @user.save
+        "User #{username} saved!"
+    else
+        erb(:signup)
+    end
 end
