b7b8c130f41472868993f6f9eb953c70ea5ca931
Brendan Azzano <brendanazzano@gmail.com>
2017-09-06 02:01:09 +0000

    Created Login/Logout pages and functions to store and redirect user on login conditions

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 get '/' do 
     @posts = Post.order(created_at: :desc)
     erb(:index)
@@ -20,8 +26,35 @@ post '/signup' do
     
     #if validations pass, save user
     if @user.save
-        "User #{username} saved!"
+        redirect to('/login')
     else
         erb(:signup)
     end
+end
+
+get '/login' do            
+    erb(:login)        
+end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+    
+    #1 find user by username
+    user = User.find_by(username: username)
+    
+    #2 if user exists
+    if user && user.password == password
+        #set user id to session
+        session[:user_id] = user.id
+        redirect to('/')
+    else
+        @error_message = "Login Failed"
+        erb(:login)
+    end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
 end
